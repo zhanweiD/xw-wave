1d5ff71fd56b0b182ad5c62abc1f973a01f31c7e
shuzhiwen <zhiwen.szw@dtwave-inc.com>
2021-08-03 11:04:39 +0800

    文件目录调整

--- src/data/index.js
+++ src/data/index.js
@@ -2,12 +2,12 @@ import Base from './base'
 import Table from './table'
 import Relation from './relation'
 import TableList from './table-list'
-import random from './random'
+import Random from './random'
 
 export default {
   Base, // 数据基类
   Table, // 表格类数据类
   Relation, // 关系类数据类
   TableList, // 列表数据类
-  random, // 随机数生成
+  Random, // 随机数生成
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Wave, {createWave} from './wave'
 import Data from './data'
 import Layer from './layer'
 import Animation from './animation'
-import layout from './layout'
+import Layout from './layout'
 import draw from './draw'
 
 export default Wave
@@ -10,7 +10,7 @@ export {
   Data, // 数据处理类的集合
   Layer, // 图层类的集合
   Animation, // 动画类的集合
-  layout, // 图表布局函数集合
+  Layout, // 图表布局函数集合
   draw, // 基础元素绘制函数集合
   createWave, // 根据 schema 创建图表实例
 }
