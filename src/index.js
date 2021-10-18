3b2b506c40883aef34b519f16eb7c30a214a97a0
shuzhiwen <zhiwen.szw@dtwave-inc.com>
2021-10-18 17:21:10 +0800

    打包成静态文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import Demo from './demo'
 import DemoOld from './demo-old'
 import './index.css'
