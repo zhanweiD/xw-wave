4d41d4c8243bacfb0c0bee297a944830e3460f54
zhiwen.szw <zhiwen.szw@dtwave-inc.com>
2021-10-27 18:35:41 +0800

    翻牌器支持自定义图片字符

--- src/demo/schema/digital-flop.js
+++ src/demo/schema/digital-flop.js
@@ -1,3 +1,5 @@
+import character from '../image/character.png'
+
 export default `[
   {
     type: 'digitalFlop',
@@ -6,8 +8,84 @@ export default `[
       layout: 'main',
     },
     data: {
-      value: 1234.456,
+      value: 12034.456,
     },
-    style: {},
+    style: {
+      character: {
+        url: "${character}",
+        0: {
+          left: 0,
+          right: 54,
+          top: 0,
+          bottom: 90,
+        },
+        1: {
+          left: 54,
+          right: 108,
+          top: 0,
+          bottom: 90,
+        },
+        2: {
+          left: 108,
+          right: 162,
+          top: 0,
+          bottom: 90,
+        },
+        3: {
+          left: 162,
+          right: 216,
+          top: 0,
+          bottom: 90,
+        },
+        4: {
+          left: 216,
+          right: 270,
+          top: 0,
+          bottom: 90,
+        },
+        5: {
+          left: 270,
+          right: 324,
+          top: 0,
+          bottom: 90,
+        },
+        6: {
+          left: 324,
+          right: 378,
+          top: 0,
+          bottom: 90,
+        },
+        7: {
+          left: 378,
+          right: 432,
+          top: 0,
+          bottom: 90,
+        },
+        8: {
+          left: 432,
+          right: 486,
+          top: 0,
+          bottom: 90,
+        },
+        9: {
+          left: 486,
+          right: 540,
+          top: 0,
+          bottom: 90,
+        },
+        ',': {
+          left: 540,
+          right: 594,
+          top: 0,
+          bottom: 90,
+        },
+        '.': {
+          left: 594,
+          right: 648,
+          top: 0,
+          bottom: 90,
+        },
+      },
+    },
   },
 ]`
--- src/layer/form-control/digital-flop.js
+++ src/layer/form-control/digital-flop.js
@@ -2,13 +2,14 @@ import {select} from 'd3'
 import {merge} from 'lodash'
 import ScrollAnimation from '../../animation/scroll'
 import DataBase from '../../data/base'
-import {range} from '../../utils/common'
+import {addStyle, range, transformAttr} from '../../utils/common'
 import LayerBase from '../base'
 
 const defaultStyle = {
   integerPlace: 8,
   decimalPlace: 2,
   thousandth: true,
+  character: {},
   text: {},
 }
 
@@ -17,7 +18,7 @@ const defaultAnimation = {
   duration: 2000,
 }
 
-const charactorSet = ['', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.']
+const characterSet = ['', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.']
 
 export default class DigitalFlopLayer extends LayerBase {
   #data = new DataBase()
@@ -58,7 +59,6 @@ export default class DigitalFlopLayer extends LayerBase {
       .style('overflow', 'hidden')
   }
 
-  // data is 2-dimensional array of object
   setData(data) {
     this.#data = this.createData('base', this.#data, data)
     this.#magnitudes = {}
@@ -113,17 +113,38 @@ export default class DigitalFlopLayer extends LayerBase {
       .data(this.#cellData)
       .join('xhtml:div')
       .attr('class', `${this.className}-group`)
-      .style('height', `${this.#data.get('cellSize')[1] * charactorSet.length}px`)
+      .style('height', `${this.#data.get('cellSize')[1] * characterSet.length}px`)
       .selectAll(`.${this.className}-text`)
-      .data(charactorSet)
+      .data(characterSet)
       .join('xhtml:div')
       .attr('class', `${this.className}-text`)
       .style('display', 'grid')
       .style('place-items', 'center')
+      .style('position', 'relative')
       .style('width', `${this.#data.get('cellSize')[0]}px`)
       .style('height', `${this.#data.get('cellSize')[1]}px`)
-      .text(d => d)
-    this.text()
+      .each((d, i, els) => {
+        if (this.#style.character[d]) {
+          // use image
+          const {left, top, bottom, right} = this.#style.character[d]
+          const [width, height] = [right - left, bottom - top]
+          const [offsetX, offsetY] = [-width / 2 - left, -height / 2 - top]
+          select(els[i])
+            .append('img')
+            .attr('src', this.#style.character.url)
+            .style('position', 'absolute')
+            .style('clip', `rect(${top}px,${right}px,${bottom}px,${left}px)`)
+            .style('transform', `translate(${offsetX}px,${offsetY}px)`)
+            .style('left', '50%')
+            .style('top', '50%')
+        } else {
+          // use font
+          const el = select(els[i])
+          const style = transformAttr(this.#style.text)
+          addStyle(el, style, i)
+          select(els[i]).text(d)
+        }
+      })
   }
 
   setAnimation(options) {
@@ -134,8 +155,8 @@ export default class DigitalFlopLayer extends LayerBase {
     const {duration, delay} = this.#animation
     this.root.selectAll(`.${this.className}-group`).each((data, i, els) => {
       if (data.text !== '') {
-        const prevIndex = charactorSet.findIndex(charactor => charactor === data.prevText)
-        const index = charactorSet.findIndex(charactor => charactor === data.text)
+        const prevIndex = characterSet.findIndex(character => character === data.prevText)
+        const index = characterSet.findIndex(character => character === data.text)
         const offset = prevIndex === -1 ? index : index - prevIndex
         const animation = new ScrollAnimation({
           delay,
@@ -150,8 +171,7 @@ export default class DigitalFlopLayer extends LayerBase {
     })
   }
 
-  // just for fun
-  text() {
+  test() {
     setTimeout(() => {
       this.setData(new DataBase({value: Math.random() * 10 ** this.#style.integerPlace}))
       this.setStyle()
